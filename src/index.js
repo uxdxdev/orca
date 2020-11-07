4b072d5f2530e51e8d67d81e4b9494a6188de39d
David Morton <davidmortonmail@gmail.com>
2020-11-07 22:54:06 +0000

    refactor(*): use access instead of refresh token

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ const snoowrap = require('snoowrap');
 const fs = require("fs");
 const { program } = require('commander');
 const chalk = require('chalk');
+const boxen = require('boxen');
 
 require('dotenv').config()
 
+const clientId = 'Ano5GFtJc_BZrVg'
+const clientSecret = 'vfAs98fP632nklizCB5Qfdsa'
+const accessToken = '70162531-R3rT0inhKaoVi1X6mNd2Ei-72dFs4hA'
+
 program
     .name("orca")
-    .usage("--user-agent=<string> --client-id=<id> --client-secret=<secret> --refresh-token=<token> --output-dir=output/")
+    .usage("--data=<string> --client-id=<id> --client-secret=<secret> --access-token=<token> --output-dir=output/")
     .helpOption('-h, --help', 'Display setup steps and options')
-    .description(`Download your Reddit upvotes, saves, and submissions.
+    .description(`${boxen(chalk.magentaBright(`Orca`), { padding: 1, margin: 1, borderStyle: 'round' })}
+    
+${chalk.bold('Download your Reddit upvotes, saves, submissions, and comments.')}
 
 Setup steps:
 - Open https://www.reddit.com/prefs/apps/
 - Click ${chalk.bold('Create another app')} button
-- Pick a name, e.g. ${chalk.bold('Orca app')}
+- Pick a name, e.g. ${chalk.bold('Orca web app')}
 - Select ${chalk.bold('web app')} as the application type
 - Set ${chalk.bold('redirect url')} to https://not-an-aardvark.github.io/reddit-oauth-helper/
 - Click ${chalk.bold('Create app')} button
@@ -24,46 +31,44 @@ Setup steps:
 
     Orca
     web app
-    ${chalk.magenta('Ano2QfJc_BZrVg')} (client id)
+    ${chalk.magenta(clientId)} (client id)
     
 - Copy your ${chalk.cyan('client secret')}, e.g.
 
-    secret ${chalk.cyan('vaSdsP6352j2nklizCB5Qfdsa')}
+    secret ${chalk.cyan(clientSecret)}
 
 - Open https://not-an-aardvark.github.io/reddit-oauth-helper/
-- Input your ${chalk.bold('client id')} and ${chalk.bold('client secret')}
-- Select ${chalk.bold('Permanent?')}
+- Input your ${chalk.bold('client id')} and ${chalk.bold('client secret')} at the top of the page
 - Select scopes
     - history
     - identity
     - read
 - Click ${chalk.bold('Generate tokens')} button
-- Copy your ${chalk.blue('refresh token')}, e.g.
+- Copy your ${chalk.blue('access token')} from the bottom of the page, e.g.
 
-    Refresh token: ${chalk.blue('70162531-R3rT0inhKaoVi1X6mNd2Ei-7BFQ4hA')}
+    Access token: ${chalk.blue(accessToken)}
 
 - Run Orca
 
-    orca --user-agent=${chalk.red('Orca app')} \\
-    --client-id=${chalk.magenta('Ano2QfJc_BZrVg')} \\
-    --client-secret=${chalk.cyan('vaSdsP6352j2nklizCB5Qfdsa')} \\
-    --refresh-token=${chalk.blue('70162531-R3rT0inhKaoVi1X6mNd2Ei-7BFQ4hA')} \\
-    --output-dir=${chalk.yellow('output/')}
-
+orca --data=${chalk.bold('upvoted,saved,submissions,comments')} \\
+--output-dir=${chalk.bold('output/')} \\
+--client-id=${chalk.magenta(clientId)} \\
+--client-secret=${chalk.cyan(clientSecret)} \\
+--access-token=${chalk.blue(accessToken)}
 `)
-    .requiredOption('--user-agent <string>', 'User agent string to use in requests to the Reddit API. e.g. Orca app')
+    .requiredOption('--data <string>', 'Data to download, e.g. upvotes,saved,submissions,comments')
+    .requiredOption('--output-dir <directory>', 'Output directory for data files (.txt)')
     .requiredOption('--client-id <id>', 'Reddit application client Id')
     .requiredOption('--client-secret <secret>', 'Reddit application client secret')
-    .requiredOption('--refresh-token <token>', 'Refresh token generated from https://not-an-aardvark.github.io/reddit-oauth-helper/')
-    .requiredOption('--output-dir <directory>', 'Output directory for data .zip file')
+    .requiredOption('--access-token <token>', 'Access token generated from https://not-an-aardvark.github.io/reddit-oauth-helper/')
 
 program.parse(process.argv);
 
 const r = new snoowrap({
-    userAgent: program.userAgent,
+    userAgent: 'Orca app',
     clientId: program.clientId,
     clientSecret: program.clientSecret,
-    refreshToken: program.refreshToken
+    accessToken: program.accessToken
 });
 
 const writeDataToTxtFile = async (directory, filename, data) => {
@@ -77,37 +82,27 @@ const writeDataToTxtFile = async (directory, filename, data) => {
     });
 }
 
-const main = async () => {
+const getSavedContent = async () => {
     const savedContent = await r.getMe().getSavedContent()
-    const upvotedContent = await r.getMe().getUpvotedContent()
-    const submissionsContent = await r.getMe().getSubmissions()
-
-    // saved content
     let savedContentData = ''
     savedContent.forEach(item => {
         savedContentData += 'https://www.reddit.com' + item.permalink + '\n'
     })
+    return savedContentData
+}
 
-    let rootOutputDirectory = program.outputDir;
-
-
-    let directory = rootOutputDirectory + '/saved/'
-    let filename = 'reddit_saved_permalinks.txt';
-    writeDataToTxtFile(directory, filename, savedContentData);
-
-    // upvoted content
+const getUpvotedContent = async () => {
+    const upvotedContent = await r.getMe().getUpvotedContent()
     let upvotedContentData = ''
     upvotedContent.forEach(item => {
         upvotedContentData += 'https://www.reddit.com' + item.permalink + '\n'
     })
+    return upvotedContentData
+}
 
-    directory = rootOutputDirectory + '/upvoted/'
-    filename = 'reddit_upvoted_permalinks.txt';
-    writeDataToTxtFile(directory, filename, upvotedContentData);
-
-
-    // submissions
-    let submissionsContentData = '';
+const getSubmissionsContent = async () => {
+    const submissionsContent = await r.getMe().getSubmissions()
+    let submissionsContentData = ''
     for (const submission of submissionsContent) {
         submissionsContentData += 'Title: ' + submission.title + '\n'
         submissionsContentData += 'Body: ' + submission.selftext + '\n'
@@ -118,13 +113,59 @@ const main = async () => {
         comments.forEach(comment => {
             submissionsContentData += '> ' + comment.body.replace(/\n/g, '') + '\n'
         })
-
         submissionsContentData += '---\n'
     }
+    return submissionsContentData
+}
 
-    directory = rootOutputDirectory + '/submissions/'
-    filename = 'reddit_submissions.txt';
-    writeDataToTxtFile(directory, filename, submissionsContentData);
+const getCommentsContent = async () => {
+    const commentsContent = await r.getMe().getComments()
+    let commentsContentData = ''
+    for (const comment of commentsContent) {
+        commentsContentData += 'Post Title: ' + comment.link_title + '\n'
+        commentsContentData += 'Comment: ' + comment.body.replace(/\n/g, '') + '\n'
+        commentsContentData += 'Link: https://www.reddit.com' + comment.permalink + '\n'
+        commentsContentData += '---\n'
+    }
+    return commentsContentData
+}
+
+const main = async () => {
+    let rootOutputDirectory = program.outputDir;
+
+    const dataToDownload = program.data.split(',');
+
+    if (dataToDownload.includes('saved')) {
+        // saved content
+        let savedContentData = await getSavedContent();
+        let directory = rootOutputDirectory + '/saved/'
+        let filename = 'reddit_saved_permalinks.txt';
+        writeDataToTxtFile(directory, filename, savedContentData);
+    }
+
+    if (dataToDownload.includes('upvoted')) {
+        // upvoted content
+        let upvotedContentData = await getUpvotedContent();
+        directory = rootOutputDirectory + '/upvoted/'
+        filename = 'reddit_upvoted_permalinks.txt';
+        writeDataToTxtFile(directory, filename, upvotedContentData);
+    }
+
+    if (dataToDownload.includes('submissions')) {
+        // submissions
+        let submissionsContentData = await getSubmissionsContent();
+        directory = rootOutputDirectory + '/submissions/'
+        filename = 'reddit_submissions.txt';
+        writeDataToTxtFile(directory, filename, submissionsContentData);
+    }
+
+    if (dataToDownload.includes('comments')) {
+        // comments
+        let commentsContentData = await getCommentsContent();
+        directory = rootOutputDirectory + '/comments/'
+        filename = 'reddit_comments.txt';
+        writeDataToTxtFile(directory, filename, commentsContentData);
+    }
 }
 
 main();
